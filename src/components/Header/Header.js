dece6df7549125d1339277f9213f059adb28a5b1
Marko Skugor <marko.skugor.1@gmail.com>
2019-01-29 18:54:27 -0500

    Adding breakpoint variables and playing around with styles some more

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import { colors } from '@config';
+import { breakpoints, colors } from '@config';
 import logo from '@images/inverted-logo.svg';
 
 const HeaderContainer = styled.div`
@@ -20,19 +20,65 @@ const HeaderText = styled.h1`
   justify-content: space-between;
   margin: 0 auto;
   max-width: 960px;
-  padding: 1.45rem 1.0875rem;
+  padding: 1rem 1rem 1.5rem;
   transform: rotate(2deg);
 
   a {
-    color: white;
-    margin-left: 0.5rem;
+    color: ${colors.brandTertiary};
+    font-size: 2rem;
+    font-weight: 900;
+    margin: 0 0.5rem 0 1rem;
+    text-transform: uppercase;
     text-align: right;
     text-decoration: none;
+    position: relative;
+    z-index: 0;
+    -webkit-text-stroke: 2px ${colors.brandPrimary}; /* webkit only */
+  
+    &:before {
+      content: attr(data-text);
+      position: absolute;
+      display: block;
+      width: 100%;
+      height: 100%;
+      top: 4px;
+      left: 4px;
+      z-index: -1;
+      background: repeating-linear-gradient(
+        0deg,
+        #fff,
+        #fff 1px,
+        ${colors.brandPrimary} 1px,
+        ${colors.brandPrimary} 2px,
+        #000 2px
+      ) top left fixed;
+      -webkit-text-fill-color: transparent; /* webkit only */
+      -webkit-background-clip: text; /* webkit only */
+      background-size: 1%;
+    }
   }
 
   img {
     margin: 0;
-    width: 10rem;
+    width: 5rem;
+  }
+
+  @media (min-width: ${breakpoints.s}) {
+    img {
+      width: 6rem;
+    }
+  }
+
+  @media (min-width: ${breakpoints.m}) {
+    padding: 1.5rem 2rem 2.5rem;
+
+    a {
+      font-size: 3rem;
+    }
+
+    img {
+      width: 10rem;
+    }
   }
 `;
 
@@ -40,7 +86,7 @@ const Header = ({ siteTitle }) => (
   <HeaderContainer>
     <HeaderText>
       <img src={logo} alt="Logo" />
-      <Link to="/">
+      <Link data-text={siteTitle} to="/" >
         {siteTitle}
       </Link>
     </HeaderText>
--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
 export const colors = {
-  brandPrimary: '#fc625f',
+  brandPrimary: '#FC625F',
+  brandSecondary: '#8E3B46',
+  brandTertiary: '#363537'
+};
+
+export const breakpoints = {
+  xs: '320px',
+  s: '450px',
+  m: '700px',
+  l: '900px',
+  xl: '1200px',
+  xxl: '1400px'
 };
